46c80fba00ce31d5fdd3e97204874cb01bf18ca3
david umoren <dxelpraise@gmail.com>
2022-04-06 18:51:16 +0100

    Add files via upload

---
+++ NewLogin/passwordhash.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>HASHING</title>
+<?php
+$input = "test123";
+echo"<br>";
+$hashedpwd = password_hash("test123", PASSWORD_DEFAULT);
+echo password_verify($input,$hashedpwd);
+
+// function generateKey(){
+
+//     $KeyLength = 8;
+//     $str = "1234567890abcdefghijklmnopqrstuvwxyz()/$";
+//     $randstr = substr(str_shuffle($str), 0, $KeyLength);
+//     return $randstr;
+// }
+
+function generateKey(){
+
+    $randstr = uniqid('daniel', true);
+     return $randstr;
+}
+echo generateKey();
+?>
+</head>
+<body>
+</body>
+</html>
